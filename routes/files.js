88e8005f81ce1914497bbec760d5c7d171075baf
Piyush Kathait <kathaitpiyush1@gmail.com>
2024-09-04 23:59:23 +0530

    na

--- routes/files.js
+++ routes/files.js
@@ -1,16 +1,6 @@
 const router = require('express').Router();
 router.post('/', (req, res) => {
-    //Validation of the request
-
-
-
-    //store the files
-
-
-    //stor in the database
-
-
-    //response includes the file download link
+    
 });
 
 module.exports = router;
