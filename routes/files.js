26d059c47d571b657e22891ba00785d934bbdbe3
Piyush-kathait <113622117+Piyush-kathait@users.noreply.github.com>
2024-09-04 23:44:31 +0530

    Add files via upload

---
+++ routes/files.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+router.post('/', (req, res) => {
+    //Validation of the request
+
+
+
+    //store the files
+
+
+    //stor in the database
+
+
+    //response includes the file download link
+});
+
+module.exports = router;
