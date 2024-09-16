d63889b6284bbb536547851f42d3dd63bef20420
Piyush-kathait <113622117+Piyush-kathait@users.noreply.github.com>
2024-09-16 10:00:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
-
+//routes
+app.use('api/files', require('./routes/files'));
 app.listen(PORT, () => {
     console.log(`listning to port ${PORT}`);
 })
