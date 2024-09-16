761f4c17295c2bfb91bb222efdf88f2254bb00ef
Piyush Kathait <kathaitpiyush1@gmail.com>
2024-09-16 10:49:31 +0530

    upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 connectDB();
 //routes
-app.use('api/files', require('./routes/files'));
+app.use('/api/files', require('./routes/files'));
 app.listen(PORT, () => {
     console.log(`listning to port ${PORT}`);
 })
